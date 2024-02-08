51a6589bca02452a64801975d8e1cdece8e06dab
Jishu <jishumandal003@gmail.com>
2024-02-08 13:23:26 +0530

    change

--- bmicalculator/index.js
+++ bmicalculator/index.js
@@ -4,9 +4,10 @@ form.addEventListener('submit', (e)=>{
     // In JavaScript, when you use document.querySelector() to select an element from the HTML document, 
     // the returned object represents the DOM (Document Object Model) element itself, not its value. 
     // To access the value of an input field or other types of elements, you need to use the .value property.
-    const height=parseInt(document.querySelector('#height').value)
+    //if anything is put on string format in the text boxes it will be converted to an integer using parseInt method.
+    const height=parseInt(document.querySelector('#height').value) 
     const weight=parseInt(document.querySelector('#weight').value)
-    const result=document.querySelector('#results')
+    const result=document.querySelector('#results') //gonna render the final result in dic with id results
     // In JavaScript, isNaN() is a built-in function used to determine whether a value is "Not-a-Number" (NaN) or not. 
     // It takes one argument, which is the value to be tested. The function returns true if the value is NaN and false 
     // if it is a valid number or can be converted into one.
@@ -17,8 +18,9 @@ form.addEventListener('submit', (e)=>{
         result.innerHTML=`the mentioned weight: ${weight} is not a valid weight`
     }
     else{
-        const bmi=(weight / ((height * height) / 10000)).toFixed(2)
-        result.innerHTML=`${bmi}`
+        //final result
+        const bmi=(weight / ((height * height) / 10000)).toFixed(2) //logic to calculate
+        result.innerHTML=`${bmi}` //show the result
     }
 
 })
